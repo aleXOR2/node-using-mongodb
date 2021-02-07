dca3f825e08ac06aae25de6c5d03c5dfa9a86a16
aleXOR2 <21229293+aleXOR2@users.noreply.github.com>
2021-02-07 19:02:53 +0100

    Task: Add MongoDB & Mongoose connection

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,3 +22,4 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
